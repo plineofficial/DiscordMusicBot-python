f3c6ac8ace7635d33f678f3219a32291cfb3d604
Romanzho <52290954+plineofficial@users.noreply.github.com>
2023-02-14 17:12:12 +0500

    rewriting the bot on the Discord Tree

--- main.py
+++ main.py
@@ -1,56 +1,78 @@
-import os
+import os 
 import discord
+from discord import app_commands
 from discord.ext import commands
-from youtubeHandler import search
+from YoutubeHandler import search
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 TOKEN = os.environ.get("BOT_TOKEN")
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
-
 playChannelInfo = {}
 soung_queue = []
 FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+	print('Im has been started')
+	try:
+		synced = await bot.tree.sync()
+		print(f'Synced {len(synced)} command(s)')
+	except Exception as e:
+		print(e)
+
+def calibration_duration(sec):
+	minutes = str(sec//60)
+	seconds = str(sec%60)
+	if int(seconds) >= 0 and int(seconds) < 10:
+		seconds = '0'+seconds
+	return f'{minutes}:{seconds}'
 
 def play_next(vc):
 	if len(soung_queue) >= 1:
 		source = soung_queue.pop(0)[1]
 		vc.play(discord.FFmpegPCMAudio(source, **FFMPEG_OPTS), after = lambda e: play_next(vc))
 
-@bot.command()
-async def play(ctx, url):
-	channel = ctx.message.author.voice
+@bot.tree.command(name='play', description='Играет/добавляет в очередь трек')
+@app_commands.describe(link='Ссылка на трек')
+async def play(interaction: discord.Interaction, link:str):
+	channel = interaction.user.voice
 	if channel:
-		if not ctx.voice_client:
+		await interaction.response.defer()
+		if not interaction.client.voice_clients:
 			await channel.channel.connect()
-		vc = ctx.guild.voice_client
+		vc = interaction.guild.voice_client
 		try:
-			song_info, source = search(url)
+			try:
+				song_info, source = search(link)
+			except:
+				await interaction.followup.send('Ошибка загруски, попробуйте еще раз!')
+				return
 			embed = discord.Embed()
 			if not vc.is_playing():
 				vc.play(discord.FFmpegPCMAudio(source, **FFMPEG_OPTS), after = lambda e: play_next(vc)) 
 				embed.title='Сейчас играет:'
 			else:
-				soung_queue.append([song_info['title'],source, url])
+				soung_queue.append([song_info['title'],source, link, song_info['duration']])
 				embed.title='Добавлено в очередь: '
 			embed.color=0xbd91e4
 			embed.add_field(
 				name=song_info['title'],
-				value=url,
-				inline=False
+				value=link+"\n\n"+f"**Длительность**: (`{calibration_duration(song_info['duration'])}`)",
+				inline=False,
 			)
 		except: 
-			await ctx.send('Произошла ошибка, попробуйте еще раз!')
+			await interaction.followup.send('Произошла ошибка, попробуйте еще раз!')
 			return
-		await ctx.send(embed=embed)
-	else: await ctx.send(f'{ctx.message.author.mention}, зайдите в гс канал!')
+		await interaction.followup.send(embed=embed)
+	else: await interaction.response.send_message(f'{interaction.user.mention}, для начала зайдите в голосовой канал!', ephemeral=True)
 
-@bot.command()
-async def skip(ctx):
-	vc = ctx.voice_client
+
+@bot.tree.command(name='skip', description='Пропускает текущий трек')
+async def skip(interaction: discord.Interaction):
+	vc = interaction.guild.voice_client
 	if vc:
 		if len(soung_queue) >= 1:
 			next_song = soung_queue[0]
@@ -60,44 +82,49 @@ async def skip(ctx):
 			)
 			embed.add_field(
 				name=next_song[0],
-				value=next_song[2],
+				value=next_song[2]+"\n\n"+f"**Длительность:** (`{calibration_duration(next_song[3])}`)",
 				inline=False
 			)
-			await ctx.send(embed=embed)	
-		else: await ctx.send('Это последняя песня, дальше ничего играть не будет')
+			embed.add_field(
+				value=calibration_duration(next_song[3])
+			)
+			await interaction.response.send_message(embed=embed)	
+		else: await interaction.response.send_message('Это была последняя песня, дальше ничего играть не будет')
 		vc.stop()
 	else:
-		await ctx.send(f'{ctx.message.author.mention}, я не подключен ни к одному из каналов!')
+		await interaction.response.send_message(f'{interaction.user.mention}, я не подключен ни к одному из каналов!', ephemeral=True)
 
-@bot.command()
-async def leave(ctx):
-	if ctx.voice_client:
-		await ctx.guild.voice_client.disconnect()
-		await ctx.send('Я вышел с канала')
+@bot.tree.command(name='leave', description='Выход из голосового канала')
+async def leave(interaction: discord.Interaction):
+	if interaction.client.voice_clients:
+		await interaction.guild.voice_client.disconnect
+		await interaction.response.send_message('Я вышел с канала')
 	else:
-		await ctx.send(f'{ctx.message.author.mention}, я не подключен ни к одному из каналов!')
+		await interaction.response.send_message(f'{interaction.user.mention}, я не подключен ни к одному из каналов!', ephemeral=True)
 
-@bot.command()
-async def pause(ctx):
-	vc = ctx.voice_client
+@bot.tree.command(name='pause', description='Остановить текущий трек')
+async def pause(interaction: discord.Interaction):
+	vc = interaction.guild.voice_client
 	if vc:
 		if vc.is_playing():
 			vc.pause()
-			await ctx.send('Иду сидеть на паузе')
-		else: await ctx.send('Я итак на паузе!')
+			await interaction.response.send_message('Иду сидеть на паузе')
+		else: await interaction.response.send_message('Я итак на паузе!')
 	else:
-		await ctx.send(f'{ctx.message.author.mention}, я не подключен ни к одному из каналов!')
+		await interaction.response.send_message(f'{interaction.user.mention}, я не подключен ни к одному из каналов!', ephemeral=True)
 
-@bot.command()
-async def resume(ctx):
-	vc = ctx.voice_client
+@bot.tree.command(name='resume', description='Продолжить проигрывание текущего трека')
+async def resume(interaction: discord.Interaction):
+	vc = interaction.guild.voice_client
 	if vc:
 		if vc.is_paused():
 			vc.resume()
-			await ctx.send('Продолжаю петь')
-		else: await ctx.send('Я итак пою!')
+			await interaction.response.send_message('Продолжаю петь')
+		else: await interaction.response.send_message('Я итак пою!')
 	else:
-		await ctx.send(f'{ctx.message.author.mention}, я не подключен ни к одному из каналов!')
+		await interaction.response.send_message(f'{interaction.user.mention}, я не подключен ни к одному из каналов!', ephemeral=True)
 
 bot.run(TOKEN)
+
+
 
